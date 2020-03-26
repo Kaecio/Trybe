f4ea19f2419c85f78855c060505b80e7d5ed7653
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-03-26 18:48:25 -0300

    exercicios ralizados

---
+++ 4_1/exercise_3.js
@@ -0,0 +1,8 @@
+let a = 8;
+let b = 9;
+let result;
+
+// multiplicação de valores das variáveis "a" e "b"
+result = a * b;
+
+console.log(result);
---
+++ 4_1/exercise_4.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 4;
+let result;
+
+// divisão de valores das variáveis "a" e "b"
+result = a / b;
+
+console.log(result);
