479bf82ea20f4380b9d879804dddf9b52d723012
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-03-30 15:46:36 -0300

    trocando para uma função de multiplicar

--- 4_1/exercise1_3.js
+++ 4_1/exercise1_3.js
@@ -1,8 +1,13 @@
-let a = 2;
-let b = 9;
-let result;
+// let a = 2;
+// let b = 9;
+// let result;
 
-//  multiplicação de valores das variáveis "a" e "b"
-result = a * b;
+// //  multiplicação de valores das variáveis "a" e "b"
+// result = a * b;
 
-console.log(result);
+// console.log(result);
+function multiplicar(a,b) {
+    return a * b
+}
+
+console.log(multiplicar(7,6))
