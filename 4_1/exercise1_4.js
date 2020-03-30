307db1f035643fcee488e7342a27abf0927a696a
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-03-30 15:47:56 -0300

    trocando para uma função de dividir

--- 4_1/exercise1_4.js
+++ 4_1/exercise1_4.js
@@ -1,9 +1,15 @@
-let a = 20;
-let b = 4;
-let result;
+// let a = 20;
+// let b = 4;
+// let result;
 
 
-// divisão de valores das variáveis "a" e "b"
-result = a / b;
+// // divisão de valores das variáveis "a" e "b"
+// result = a / b;
 
-console.log(result);
+// console.log(result);
+
+function dividir(a,b) {
+    return a / b
+}
+
+console.log(dividir(10,6))
