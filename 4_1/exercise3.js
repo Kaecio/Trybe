064a335cc96c365b9aebbc44da1fe5c2c3ce68a1
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-03-29 22:49:44 -0300

    Descobrir entre as tres variáveis qual é a maior

---
+++ 4_1/exercise3.js
@@ -0,0 +1,11 @@
+let a = 4;
+let b = 8;
+let c = 12;
+
+if (a > b && a > c) {
+    console.log("A é maior que B e C")
+} else if (b > c && b > a){
+    console.log("B é maior que A e C")
+} else{
+    console.log("C é maior que B e A")
+}
