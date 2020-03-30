94ba07356fcb660b80fea0883799cac93bdc3ad7
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-03-30 15:41:45 -0300

    tentando fazer o exercicio

--- 4_1/exercise1_1.js
+++ 4_1/exercise1_1.js
@@ -1,8 +1,14 @@
-let a = 4;
+/*let a = 4;
 let b = 11;
 let result;
 
 //  soma de valores das variáveis "a" e "b"
 result = a + b;
 
-console.log(result);
+console.log(result);*/
+
+function somar(a,b) {
+    return a + b
+}
+
+console.log(somar(2,6))
