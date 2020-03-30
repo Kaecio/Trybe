c1714e099858ce54cf40647ee99cdc3ebafcafaf
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-03-29 21:46:49 -0300

    multiplicação de valores das variáveis a e b

--- 4_1/exercise1_3.js
+++ 4_1/exercise1_3.js
@@ -2,7 +2,7 @@ let a = 2;
 let b = 9;
 let result;
 
-// multiplicação de valores das variáveis "a" e "b"
+//  multiplicação de valores das variáveis "a" e "b"
 result = a * b;
 
 console.log(result);
