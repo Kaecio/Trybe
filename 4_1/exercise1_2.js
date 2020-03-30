745c2271d5163d72288cdf656c4a3def2dc330e4
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-03-30 15:45:25 -0300

    trocando para uma função de subtrair

--- 4_1/exercise1_2.js
+++ 4_1/exercise1_2.js
@@ -1,8 +1,14 @@
-let a = 8;
-let b = 9;
-let result;
+// let a = 8;
+// let b = 9;
+// let result;
 
-//  subtração de valores das variáveis "a" e "b"
-result = a - b;
+// //  subtração de valores das variáveis "a" e "b"
+// result = a - b;
 
 console.log(result);
+
+function subtrair(a,b) {
+    return a - b
+}
+
+console.log(subtrair(8,1))
