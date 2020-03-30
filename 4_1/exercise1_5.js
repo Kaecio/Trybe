56622b2ef1393b5c700885efbd71141e96781892
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-03-30 15:48:52 -0300

    trocando para uma função de resto

--- 4_1/exercise1_5.js
+++ 4_1/exercise1_5.js
@@ -1,9 +1,15 @@
-let a = 7;
-let b = 3;
-let result;
+// let a = 7;
+// let b = 3;
+// let result;
 
 
-//  mod de valores das variáveis "a" e "b"
-result = a % b;
+// //  mod de valores das variáveis "a" e "b"
+// result = a % b;
 
-console.log(result);
+// console.log(result);
+
+function resto(a,b) {
+    return a % b
+}
+
+console.log(resto(14,6))
