c4214c551d228f756f1fa317265ce2ef687d7f50
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-03-31 18:36:46 -0300

    primeiro exercicio

---
+++ 4_3/exercise1_part2.js
@@ -0,0 +1,9 @@
+function verificaPalindrome() {
+    let nome = ["arara"]
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+}
+console.log(verificaPalindrome())
